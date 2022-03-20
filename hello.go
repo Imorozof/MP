a45294b1f81feb3bd24a348628b087af838727d3
DeCased@bk.ru <Lvq2V#Q2>
2022-03-20 23:58:47 +0300

    123

--- hello.go
+++ hello.go
@@ -5,8 +5,11 @@ import "fmt"
 func main() {
 
 	var (
-		testVariable  = "Hello, world!"
-		testVariable2 = 23
+		a, b, c int
 	)
-	fmt.Println(testVariable, ",", testVariable2)
+	fmt.Scan(&a) // считаем переменную 'a' с консоли
+	c = a / 30
+	b = 2 * (a % 30)
+
+	fmt.Println("It is", c, "hours", b, "minutes.")
 }
