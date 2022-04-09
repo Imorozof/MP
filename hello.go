5266df235bda4ab79111f23ede3c5ed1b56a3a53
DeCased@bk.ru <Lvq2V#Q2>
2022-04-09 21:50:14 +0300

    задача со степика про цикл

--- hello.go
+++ hello.go
@@ -3,14 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var newNumber int8
-	for fmt.Scan(&newNumber); newNumber > 0; fmt.Scan(&newNumber) {
-		if newNumber < 10 {
-			continue
-		} else if newNumber >= 10 && newNumber <= 100 {
-			fmt.Println(newNumber)
-		} else if newNumber > 100 {
-			break
-		}
+	var x, p, y, n int
+	fmt.Scan(&x, &p, &y)
+	for n = 0; x <= y; n++ {
+		x += x * p / 100
 	}
+	fmt.Println(n)
 }
